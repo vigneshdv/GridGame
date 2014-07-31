dc3d1b844d7615f4f71f8a8245cc36058d6dc7a1
unknown <vidv@.corp.ebay.com>
2014-07-31 16:26:42 +0530

    snail updated

--- Snail.java
+++ Snail.java
@@ -11,9 +11,18 @@ package trainingday4;
  * @author vidv
  */
 public class Snail {
+    public static final long DIRECTION_NORTH = 0;
+    public static final long DIRECTION_EAST = 1;
+    public static final long DIRECTION_SOUTH = 2;
+    public static final long DIRECTION_WEST = 3;
     
     private Tile currentTile;
+    private long currentDirection;
     
+    public Snail(Tile tile, long direction) {
+        this.currentTile = tile;
+        this.currentDirection = direction;
+    }
     
     public void toggleColor() {
         if (currentTile.getColor() == Tile.COLOR_WHITE) {
@@ -24,16 +33,24 @@ public class Snail {
     }
     
     public void moveForward() {
-        
+       if (this.currentDirection == Snail.DIRECTION_NORTH) {
+           this.currentTile.setY(this.currentTile.getY() - 1L);
+       } else if (this.currentDirection == Snail.DIRECTION_SOUTH) {
+           this.currentTile.setY(this.currentTile.getY() + 1L);
+       } else if (this.currentDirection == Snail.DIRECTION_EAST) {
+           this.currentTile.setX(this.currentTile.getX() + 1L);
+       } else if (this.currentDirection == Snail.DIRECTION_WEST) {
+           this.currentTile.setX(this.currentTile.getX() - 1L);
+       }
         
     }
     
     public void turnRight() {
-        
+       this.currentDirection = ( this.currentDirection + 1L) % 4L; 
     }
     
     public void turnLeft() {
-        
+        this.currentDirection = ( this.currentDirection - 1L) % 4L; 
     }
     
 }
